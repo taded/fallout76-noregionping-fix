f1b4d35eabac7600f90cd24473488ef93dc67fff
taded <68170322+taded@users.noreply.github.com>
2020-07-11 17:08:25 -0700

    Update fo76.bat

--- fo76.bat
+++ fo76.bat
@@ -1,4 +1,4 @@
 @echo off
-xcopy /i "D:\Program Files\ModifiableWindowsApps\Fallout 76\Project76.ini" "C:\Users\Fletc\Documents\My Games\Fallout 76\" /Y
+xcopy /i "D:\WindowsApps\BethesdaSoftworks.Fallout76-PC_1.0.5.0_x64__3275kfvn8vcwc\Project76" "C:\Users\Fletc\Documents\My Games\Fallout 76\" /Y
 attrib +R "C:\Users\Fletc\Documents\My Games\Fallout 76\Project76.ini"
 start "" "C:\Users\Fletc\Documents\My Games\Fallout 76"
